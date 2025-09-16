71de8fe81dee52d57140c1c28d0dcce409a269e0
JaimeAndia <andiacintorajaime@gmail.com>
2025-09-16 11:10:04 +0200

    Ejercicio 4

--- src/04_Operadores.kt
+++ src/04_Operadores.kt
@@ -9,4 +9,33 @@ fun main(){
     println("($a / $b) = ${a / b}")
     println("($a * $b) = ${a * b}")
     println("($a % $b) = ${a % b}")
+
+    val input = 5
+    var result: Boolean
+
+    val greaterThanZero = input > 0
+    val isEven = input % 2 == 0
+
+    result = greaterThanZero && isEven
+    println("Es mayor que cero y par:$result")
+
+    result = greaterThanZero || isEven
+    println("Es mayor que cero o par:$result")
+
+    result = greaterThanZero && !isEven
+    println("Es mayor que cero e impar:$result")
+
+    a = 10
+    b = 20
+
+    a += b
+    println("+= $a")
+    a -= b
+    println("-= $a")
+    a *= b
+    println("*= $a")
+    a /= b
+    println("/= $a")
+    a %= b
+    println("%= $a")
 }
