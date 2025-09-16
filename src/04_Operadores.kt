5e0bb26f75e397ee192c4952c0eb627e2ac3ec1b
JaimeAndia <andiacintorajaime@gmail.com>
2025-09-16 10:13:00 +0200

    Ejercicio 4

---
+++ src/04_Operadores.kt
@@ -0,0 +1,12 @@
+fun main(){
+
+    var a = 7
+    var b = 5
+
+    println("($a + $b) = ${a + b}") //Con el $ se recupera el valor de la variable
+    println("(a + b) = ${a + b}")
+    println("($a - $b) = ${a - b}")
+    println("($a / $b) = ${a / b}")
+    println("($a * $b) = ${a * b}")
+    println("($a % $b) = ${a % b}")
+}
