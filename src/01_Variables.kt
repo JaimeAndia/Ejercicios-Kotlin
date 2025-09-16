99810d6f5a34699d8cf0700c4dfa573594d32a84
JaimeAndia <andiacintorajaime@gmail.com>
2025-09-16 08:40:19 +0200

    Ejercicio 1

---
+++ src/01_Variables.kt
@@ -0,0 +1,25 @@
+fun main() {
+
+    var nombre = "Michel"
+    var numero = 123
+
+    println(nombre)
+    println(numero)
+
+    nombre = ""+2
+    println(nombre)
+
+    var cantidad : Int
+    cantidad = 33
+    var nota : Double
+    nota = 7.77
+    var letra : Char
+    letra = 'a'
+    var existe : Boolean
+    existe = true
+
+    println(cantidad)
+    println(nota)
+    println(letra)
+    println(existe)
+}
