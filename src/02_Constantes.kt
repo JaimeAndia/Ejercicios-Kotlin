9d4ae3f70eaa6ab7e920316c6194e9c9f628a675
JaimeAndia <andiacintorajaime@gmail.com>
2025-09-16 09:18:53 +0200

    Merge remote-tracking branch 'origin/master'

---
+++ src/02_Constantes.kt
@@ -0,0 +1,13 @@
+const val FICHERO = "Datos.txt"
+
+fun main() {
+
+    val fichero = "datos.txt"
+    val xPos = 1
+    val yPos = 2
+
+    println(FICHERO)
+    println(xPos)
+    println(yPos)
+    println(fichero)
+}
