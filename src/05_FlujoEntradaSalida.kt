01d513246186606f412500f0884458f0a012ec14
JaimeAndia <andiacintorajaime@gmail.com>
2025-09-18 11:38:36 +0200

    Ejercicio 4

---
+++ src/05_FlujoEntradaSalida.kt
@@ -0,0 +1,12 @@
+fun main(){
+    val level = "Subiste de nivel!"
+    println(level)
+
+    println("Escribe tu nombre")
+    val name = readln()
+
+    println("Escribe tu apellido")
+    val surname = readln()
+
+    print("Bienvenido $name $surname")
+}
